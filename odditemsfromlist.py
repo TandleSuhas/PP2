63f2ce3de1355a6c2c5876217b329d50fbdc3f9e
suhas_tandle <130026611+TandleSuhas@users.noreply.github.com>
2024-02-08 23:04:37 +0530

    Update odditemsfromlist.py

--- odditemsfromlist.py
+++ odditemsfromlist.py
@@ -1,2 +1,7 @@
-x=[1,2,3,4,5,6,7,8,9]
-print(x[::2])
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+odd_items = []
+
+for item in my_list:
+    if item % 2 != 0:
+        odd_items.append(item)
+print("Odd items from the list:", odd_items)
